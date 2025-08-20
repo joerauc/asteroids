2931088c0bc72d2c29739ba9b8b7e0d69878dec8
joerauc <99369956+joerauc@users.noreply.github.com>
2025-08-19 21:19:59 -0500

    Added refresh rate and displaying a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 
 def main():
     print("Starting Asteroids!")
@@ -7,8 +8,11 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
     pygame.init()
-    
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player_one = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     playing = True
     while(playing):
@@ -16,7 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player_one.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
