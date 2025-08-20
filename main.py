570950303ee4d4c908479bb8e2d1affa5f40b0c1
joerauc <99369956+joerauc@users.noreply.github.com>
2025-08-19 21:27:05 -0500

    Cleaned up imports and associated variables/classes in player.py and main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pygame
-import constants
-import player
+from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
-    player_one = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     playing = True
     while(playing):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
         player_one.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 import pygame
-import circleshape
-import constants
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
 
-class Player(circleshape.CircleShape):
+class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y, constants.PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
     def triangle(self):
